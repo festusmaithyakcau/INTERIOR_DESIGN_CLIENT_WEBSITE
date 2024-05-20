304d430c9af738a04aa13b911415e1a493f5736b
Festus Maithya <festusmaithya264@gmail.com>
2024-05-20 12:10:53 +0300

    Resolves the styling issue for the other pages

--- .trunk/tools/prettier.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo There was an error while installing prettier. See .trunk/out/r0Cvy.yaml for details.
-exit /b 1;
--- .trunk/tools/svgo.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo There was an error while installing svgo. See .trunk/out/J5kmT.yaml for details.
-exit /b 1;
